b2addcaa7900a93b2df6ab296298794d6411e95d
josolha <josolha@nate.com>
2023-01-05 15:16:49 +0900

    문제풀이

---
+++ src/LEVEL1/문자열나누기.java
@@ -0,0 +1,15 @@
+package LEVEL1;
+
+public class 문자열나누기 {
+    public static void main(String[] args) {
+        문자열나누기 문자열나누기 = new 문자열나누기();
+        String s = "banana";
+        문자열나누기.solution(s);
+    }
+    public int solution(String s) {
+        int answer = 0;
+
+        return answer;
+    }
+
+}

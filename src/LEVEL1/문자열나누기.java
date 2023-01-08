a7c29fe9d65da5c01b27ccbb186a88a4446d1ab2
josolha <josolha@nate.com>
2023-01-08 21:56:21 +0900

    문제풀이

--- src/LEVEL1/문자열나누기.java
+++ src/LEVEL1/문자열나누기.java
@@ -4,12 +4,31 @@ public class 문자열나누기 {
     public static void main(String[] args) {
         문자열나누기 문자열나누기 = new 문자열나누기();
         String s = "banana";
-        문자열나누기.solution(s);
+        String s1 = "abracadabra";
+        String s2 = "aaabbaccccabba";
+//        System.out.println("s = " + s.substring(1));
+
+        문자열나누기.solution(s2);
     }
-    public int solution(String s) {
+
+    public static int solution(String s) {
+        char first = s.charAt(0); // 첫번쨰 문자
+        int count = 0;
+        int diff = 0;
         int answer = 0;
 
+        for (int i = 0; i < s.length(); i++) {
+            if (first == s.charAt(i)) { //
+                count++;
+            }else {
+                diff++;
+            }
+            if (count == diff) {
+                answer++;
+                first = s.charAt(i); //b
+            }
+        }
+        System.out.println("answer = " + answer);
         return answer;
     }
-
 }

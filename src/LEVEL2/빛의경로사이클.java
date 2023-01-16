ca98652494f160080c8fd963172d29c19efaaadd
josolha <josolha@nate.com>
2023-01-16 23:30:49 +0900

    문제풀이

---
+++ src/LEVEL2/빛의경로사이클.java
@@ -0,0 +1,13 @@
+package LEVEL2;
+
+public class 빛의경로사이클 {
+    public static void main(String[] args) {
+        빛의경로사이클 answer = new 빛의경로사이클();
+        String[] grid = {"SL","LR"};
+        answer.solution(grid);
+    }
+    public int[] solution(String[] grid) {
+        int[] answer = {};
+        return answer;
+    }
+}

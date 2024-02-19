96c7daf6461a5ff0e8ed97b0c920ef28b19c5f81
josolha <josolha@nate.com>
2024-02-19 11:28:33 +0900

    문제풀이

--- src/softeer/효도음식.java
+++ src/softeer/효도음식.java
@@ -5,39 +5,41 @@ import java.util.Scanner;
 
 public class 효도음식 {
 
-    static int[] arr;
-    static int a;
-
-    static boolean[] visited;
-
-    static int[] combination;
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        a = scanner.nextInt();
-        arr = new int[a];
-        visited = new boolean[a];
-        combination = new int[a];
-
-        for (int i = 0; i <a ; i++) {
-            arr[i] = scanner.nextInt();
+        int n = scanner.nextInt();
+        int[] preferences = new int[n];
+        for (int i = 0; i < n; i++) {
+            preferences[i] = scanner.nextInt();
         }
 
-        dfsCombination(0);
+        // 접두사 합과 접미사 합을 위한 Kadane 알고리즘 활용
+        int[] prefixMax = new int[n];
+        int[] suffixMax = new int[n];
+        calculateMaxSubarraySum(preferences, prefixMax, suffixMax);
 
+        // 최대 만족도 계산
+        int maxSatisfaction = Integer.MIN_VALUE;
+        for (int i = 1; i < n - 1; i++) {
+            maxSatisfaction = Math.max(maxSatisfaction, prefixMax[i] + suffixMax[i + 1]);
+        }
+
+        System.out.println(maxSatisfaction);
     }
-    private static void dfsCombination(int index) {
-        if (index == a){
-            System.out.println(Arrays.toString(combination)); // 배열 출력
-            return;
+
+    private static void calculateMaxSubarraySum(int[] preferences, int[] prefixMax, int[] suffixMax) {
+        int currentMax = preferences[0];
+        prefixMax[0] = preferences[0];
+        for (int i = 1; i < preferences.length; i++) {
+            currentMax = Math.max(preferences[i], currentMax + preferences[i]);
+            prefixMax[i] = Math.max(prefixMax[i - 1], currentMax);
         }
-        for (int i = 0; i <a; i++) {
-            if(!visited[i]){
-                visited[i] = true;
-                combination[index] = arr[i];
-                dfsCombination( index + 1); // 여기서 dfs 호출 결과를 total에 누적
-                visited[i] = false;
-            }
+
+        currentMax = preferences[preferences.length - 1];
+        suffixMax[preferences.length - 1] = preferences[preferences.length - 1];
+        for (int i = preferences.length - 2; i >= 0; i--) {
+            currentMax = Math.max(preferences[i], currentMax + preferences[i]);
+            suffixMax[i] = Math.max(suffixMax[i + 1], currentMax);
         }
     }
 }

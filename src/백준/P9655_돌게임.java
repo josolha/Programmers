89b0cc6405f316f29be7b13b8651d8fff951a4f0
josolha <josolha@nate.com>
2024-03-07 10:03:48 +0900

    문제풀이

--- src/백준/P9655_돌게임.java
+++ src/백준/P9655_돌게임.java
@@ -1,4 +1,16 @@
 package 백준;
 
+import java.util.Scanner;
+
 public class P9655_돌게임 {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int a =sc.nextInt();
+
+        if(a%2==0){
+            System.out.println("CY");
+        }else{
+            System.out.println("SK");
+        }
+    }
 }

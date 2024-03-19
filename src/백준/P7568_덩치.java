d2bfa79e0e5d50b3bdd80f6c0130b6f48e647664
josolha <josolha@nate.com>
2024-03-19 23:11:42 +0900

    문제풀이

--- src/백준/P7568_덩치.java
+++ src/백준/P7568_덩치.java
@@ -29,18 +29,26 @@ public class P7568_덩치 {
             int[] arr = new int[2];
             arr[0] = Integer.parseInt(st.nextToken());
             arr[1] = Integer.parseInt(st.nextToken());
-            hashMap.put(i+1,arr);
+            hashMap.put(i,arr);
         }
-        for (Integer keys:
-             hashMap.keySet()) {
-            System.out.println(keys+" "+Arrays.toString(hashMap.get(keys)));
+//        for (Integer keys:
+//             hashMap.keySet()) {
+//            System.out.println(keys+" "+Arrays.toString(hashMap.get(keys)));
+//        }
+        for (int i = 0; i < hashMap.size() ; i++) {
+            int cnt =1;
+            for (int j = 0; j < hashMap.size() ; j++) {
+                if(hashMap.get(i)[0] < hashMap.get(j)[0] && hashMap.get(i)[1] < hashMap.get(j)[1]){
+                    cnt++;
+                }
+            }
+            rank[i] = cnt;
+        }
+        for (int i = 0; i < rank.length ; i++) {
+            System.out.print(rank[i]+" ");
         }
 
-        Collections.sort(hashMap, new Comparator<>() {
-            @Override
-            public int compare(Object o1, Object o2) {
-                return 0;
-            }
-        });
+        //System.out.println("Arrays.toString(rank) = " + Arrays.toString(rank));
+
     }
 }

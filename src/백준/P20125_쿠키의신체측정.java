8b05dd4d7c504223ed63864957211785477561de
josolha <josolha@nate.com>
2024-04-03 22:20:08 +0900

    문제풀이

---
+++ src/백준/P20125_쿠키의신체측정.java
@@ -0,0 +1,11 @@
+package 백준;
+
+public class P20125_쿠키의신체측정 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ src/백준/P25757_임스와함께하는미니게임.java
@@ -0,0 +1,71 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class P25757_임스와함께하는미니게임 {
+    
+    static String[] names;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        int memberCnt = Integer.parseInt(st.nextToken());
+        String gameType = st.nextToken();
+        names = new String[memberCnt];
+
+        for (int i = 0; i <memberCnt ; i++) {
+            st = new StringTokenizer(br.readLine());
+            names[i] = st.nextToken();
+        }
+
+       // System.out.println("Arrays.toString(names) = " + Arrays.toString(names));
+
+        switch (gameType){
+            case "Y":
+                System.out.println(yGame());
+                break;
+            case "F":
+                System.out.println(fGame());
+                break;
+            case "O":
+                System.out.println(oGame());
+                break;
+        }
+    }
+    static int yGame(){
+        Set set = new HashSet<String>();
+        for (String name: names) {
+            set.add(name);
+        }
+        return set.size();
+    }
+    static int fGame(){
+        Set set = new HashSet<String>();
+        for (String name: names) {
+            set.add(name);
+        }
+        return set.size() / 2;
+    }
+    static int oGame(){
+        Set set = new HashSet<String>();
+        for (String name: names) {
+            set.add(name);
+        }
+        return set.size() / 3;
+    }
+
+
+
+}
